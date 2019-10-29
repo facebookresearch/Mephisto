e90099ee56863b798dd6c429a49fb19f0c609ed4
Pratik Ringshia <tikir@fb.com>
2019-10-29 19:09:59 -0400

    Wire up cmd to a group instead of actual command

--- mephisto/cli/client.py
+++ mephisto/cli/client.py
@@ -1,14 +1,16 @@
 import click
 from mephisto.cli.server import app
+from click_default_group import DefaultGroup
 
-@click.group()
+@click.group(cls=DefaultGroup, default='web', default_if_no_args=True)
 def cli():
 	pass
 
-@cli.command()
+@cli.command('web')
 def web():
+	"""Launch a local webserver with the Mephisto UI"""
 	app.run(debug=False)
 
 if __name__ == '__main__':
-	web()
+	cli()
 	
