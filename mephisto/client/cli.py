376c54add42c5d5945427936a1364d96d5a285d2
Jack Urbanek <jju@fb.com>
2019-10-31 14:52:10 -0400

    missing blacked files

--- mephisto/client/cli.py
+++ mephisto/client/cli.py
@@ -2,15 +2,17 @@ import click
 from mephisto.client.server import app
 from click_default_group import DefaultGroup
 
-@click.group(cls=DefaultGroup, default='web', default_if_no_args=True)
+
+@click.group(cls=DefaultGroup, default="web", default_if_no_args=True)
 def cli():
-	pass
+    pass
 
-@cli.command('web')
+
+@cli.command("web")
 def web():
-	"""Launch a local webserver with the Mephisto UI"""
-	app.run(debug=False)
+    """Launch a local webserver with the Mephisto UI"""
+    app.run(debug=False)
 
-if __name__ == '__main__':
-	cli()
-	
+
+if __name__ == "__main__":
+    cli()
