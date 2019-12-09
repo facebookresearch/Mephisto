d9a158e2587d1a70f569505145d3f403cd128f61
Pratik <tikir@fb.com>
2019-12-09 16:39:35 -0500

    Check in build assets so mephisto cli can instantly run (#25)

---
+++ mephisto/webapp/build/precache-manifest.2e99e22d130a15f0546cac580af192dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520f49c40a4af1590ecf5f6761f074ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94d5985dccd8911d8a58",
+    "url": "/static/css/main.4b3416f3.chunk.css"
+  },
+  {
+    "revision": "54446ba46b7001c87af6",
+    "url": "/static/js/2.c380e4ee.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/static/js/2.c380e4ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94d5985dccd8911d8a58",
+    "url": "/static/js/main.f47de31a.chunk.js"
+  },
+  {
+    "revision": "e0acb5af05f59d50df01",
+    "url": "/static/js/runtime-main.3d6e97dd.js"
+  },
+  {
+    "revision": "cf23121829070fedf98e5caa6585792f",
+    "url": "/static/media/M.cf231218.svg"
+  },
+  {
+    "revision": "44730b5bd732ae067bd4f9f8abe88757",
+    "url": "/static/media/sample.44730b5b.png"
+  }
+]);
