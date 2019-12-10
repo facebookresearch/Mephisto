8fd141b42af5e06d0b0c7f6ed5bd565f2ec39201
Pratik Ringshia <tikir@fb.com>
2019-12-10 14:49:13 -0500

    remove build folder from .gitignore

---
+++ mephisto/webapp2/build/precache-manifest.4987f4e1b76609a2d91e5b859798eff2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e7a8afc00d2aa85c47f62d999c5bfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7abe279d4f084ace8c81",
+    "url": "/static/css/2.d6c513eb.chunk.css"
+  },
+  {
+    "revision": "5309d8afb3e0aef9a5fe",
+    "url": "/static/css/main.2b44fa5b.chunk.css"
+  },
+  {
+    "revision": "7abe279d4f084ace8c81",
+    "url": "/static/js/2.7f2a0443.chunk.js"
+  },
+  {
+    "revision": "d23dc9249aea100a64a108087f61bbbc",
+    "url": "/static/js/2.7f2a0443.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5309d8afb3e0aef9a5fe",
+    "url": "/static/js/main.4cc6b556.chunk.js"
+  },
+  {
+    "revision": "8053f494ff8693034744",
+    "url": "/static/js/runtime-main.e0f99456.js"
+  },
+  {
+    "revision": "456d8835929d40c7390a65b75b068863",
+    "url": "/static/media/Oval.456d8835.svg"
+  },
+  {
+    "revision": "782154b83b6b419269980b375144b1f7",
+    "url": "/static/media/icons-16.782154b8.ttf"
+  },
+  {
+    "revision": "7fb844a70d4ff3aa49441242b4149777",
+    "url": "/static/media/icons-16.7fb844a7.woff"
+  },
+  {
+    "revision": "d4f6722b1ac527dd937155d419ec8ddd",
+    "url": "/static/media/icons-16.d4f6722b.eot"
+  },
+  {
+    "revision": "90afdd237a469c44c9a1a3b0ff2db197",
+    "url": "/static/media/icons-20.90afdd23.eot"
+  },
+  {
+    "revision": "c0e4874e23b8c9100e566ca2ca287128",
+    "url": "/static/media/icons-20.c0e4874e.woff"
+  },
+  {
+    "revision": "d1f0f6c1a1c239a03914f7bd06ccfe68",
+    "url": "/static/media/icons-20.d1f0f6c1.ttf"
+  }
+]);
