a2042280bccf4119872ffd792b726808a1640d35
Pratik Ringshia <tikir@fb.com>
2019-12-15 20:30:45 -0500

    Refactor out into <TaskRunSummary />

--- mephisto/webapp/src/widgets/Launch.tsx
+++ mephisto/webapp/src/widgets/Launch.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import BaseWidget from "./Base";
 import useAxios from "axios-hooks";
-import cx from "classnames";
 import { TaskRun, RunningTasks } from "../models";
 import { task_runs__running } from "../mocks";
-import { Card, Colors, Tag, Intent, Icon } from "@blueprintjs/core";
-import moment from "moment";
-import { createAsync, ResponseValues, mockRequest } from "../lib/Async";
+import { Colors, Icon } from "@blueprintjs/core";
+import { createAsync, mockRequest } from "../lib/Async";
+import TaskRunSummary from "./TaskRunSummary";
 
 const Async = createAsync<RunningTasks>();
 
@@ -35,56 +34,7 @@ export default (function LaunchWidget() {
         onData={({ data }) => (
           <div>
             {data.task_runs.map((run: TaskRun) => (
-              <div
-                className="run-header"
-                style={{
-                  backgroundColor: Colors.LIGHT_GRAY5,
-                  padding: 10,
-                  marginBottom: "10px",
-                  borderColor: Colors.GRAY5,
-                  borderWidth: 1,
-                  borderStyle: "solid",
-                  borderRadius: 5
-                }}
-              >
-                <h5 className="bp3-heading" style={{ display: "inline" }}>
-                  {run.task_name}
-                </h5>{" "}
-                &mdash; {moment(run.start_time).fromNow()}
-                <Tag
-                  icon="play"
-                  intent={Intent.SUCCESS}
-                  interactive={false}
-                  style={{ float: "right", marginLeft: 10 }}
-                >
-                  Running
-                </Tag>
-                <code
-                  className="bp3-code params-list"
-                  style={{ display: "block", marginTop: 10 }}
-                >
-                  {run.param_string}
-                </code>
-                <div className="details">
-                  <div className="metrics highlight-first">
-                    <div className="metric">
-                      {run.task_status.created + run.task_status.launched}
-                      <label>Remaining</label>
-                    </div>
-                    <div className="metric">
-                      {run.task_status.completed +
-                        run.task_status.accepted +
-                        run.task_status.mixed +
-                        run.task_status.rejected}
-                      <label>Completed</label>
-                    </div>
-                    <div className="metric">
-                      {run.task_status.launched + run.task_status.assigned}
-                      <label>In-Flight</label>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <TaskRunSummary run={run} />
             ))}
           </div>
         )}
