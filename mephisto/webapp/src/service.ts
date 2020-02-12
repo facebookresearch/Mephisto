f6b81ea687ebeb4077a2cc3aa23a3ec15ddb2d5a
Pratik Ringshia <tikir@fb.com>
2020-02-12 12:18:52 -0500

    Wire up interface for launching a task

--- mephisto/webapp/src/service.ts
+++ mephisto/webapp/src/service.ts
@@ -1,5 +1,33 @@
 import axios from "./axios";
 
 export function launchTask(data: any) {
-  axios.post("launch_task_run", data);
+  const transformedPayload = mapDataToExpectedPayload(data);
+  axios.post("task_runs/launch", transformedPayload);
+}
+
+function mapDataToExpectedPayload(data: any) {
+  const transformed = Object.entries(data).map(([key, value]) => {
+    if (key === "blueprint") {
+      return [
+        "blueprint_type",
+        {
+          option_string: "--blueprint_type",
+          value: value
+        }
+      ];
+    } else if (key === "architect") {
+      return [
+        "architect_type",
+        {
+          option_string: "--architect_type",
+          value: value
+        }
+      ];
+    } else {
+      const [namespace, arg_name, opt_string] = key.split("|");
+      return [arg_name, { option_string: opt_string, value: value }];
+    }
+  });
+
+  return Object.fromEntries(transformed);
 }
