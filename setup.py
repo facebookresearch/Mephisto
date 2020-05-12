29858c160b7758f9baa314f566ab43dcffde4ef6
Jack Urbanek <jju@fb.com>
2020-05-12 17:06:24 -0400

    Adding setup.py for local pip install

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="mephisto",
+    version="0.1",
+    packages=find_packages(),
+    # package_dir={'': 'mephisto'},
+)
