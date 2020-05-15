ea6dbff04dac525c97ac1ef2720fedebb51c54d5
Jack Urbanek <jju@fb.com>
2020-05-15 17:52:36 -0400

    Cleaning up examples with a parser

--- examples/simple_static_task/static_test_script.py
+++ examples/simple_static_task/static_test_script.py
@@ -1,41 +1,17 @@
 import os
 import time
 import shlex
-from mephisto.core.local_database import LocalMephistoDB
 from mephisto.core.operator import Operator
 from mephisto.core.utils import get_root_dir
 from mephisto.server.blueprints.static_task.static_html_blueprint import BLUEPRINT_TYPE
+from mephisto.utils.scripts import MephistoRunScriptParser
 
-USE_LOCAL = True
-
-db = LocalMephistoDB()
+architect_type, requester_name, db, args = MephistoRunScriptParser().parse_launch_arguments()
 
 TASK_DIRECTORY = os.path.join(get_root_dir(), "examples/simple_static_task")
 
 task_title = "Test static task"
 task_description = "This is a simple test of static tasks."
-
-provider_type = "mock" if USE_LOCAL else "mturk_sandbox"
-architect_type = "local" if USE_LOCAL else "heroku"
-
-# The first time round, need to call the following here.
-# TODO(#95) make this more user friendly than needing to uncomment script lines
-# db.new_requester("<mturk_account_name>", "mturk")
-# db.new_requester("<mturk_account_name>_sandbox", "mturk_sandbox")
-
-if USE_LOCAL:
-    from mephisto.core.utils import get_mock_requester
-
-    requester = get_mock_requester(db)
-else:
-    requester = db.find_requesters(provider_type=provider_type)[-1]
-requester_name = requester.requester_name
-assert USE_LOCAL or requester_name.endswith(
-    "_sandbox"
-), "Should use a sandbox for testing"
-
-# The first time using mturk, need to call the following here
-# requester.register()
 
 ARG_STRING = (
     f"--blueprint-type {BLUEPRINT_TYPE} "
