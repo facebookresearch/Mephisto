4bb32a0d8444816421d5e7b063f9ad2c7404dcd4
Pratik Ringshia <tikir@fb.com>
2020-05-26 16:13:28 -0400

    Update var name

--- packages/mephisto-task/src/live.js
+++ packages/mephisto-task/src/live.js
@@ -23,8 +23,8 @@ const useMephistoLiveTask = function (props) {
   const [agentState, setAgentState] = React.useState(null);
   const [agentStatus, setAgentStatus] = React.useState(null);
 
-  const defaultMephistoSocket = useMephistoSocket;
-  const useConnectionHook = props.customConnectionHook || defaultMephistoSocket;
+  const defaultConnectionHook = useMephistoSocket;
+  const useConnectionHook = props.customConnectionHook || defaultConnectionHook;
 
   const taskProps = useMephistoTask();
   const socketProps = useConnectionHook({
