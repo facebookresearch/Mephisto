a32b9941303ae12b4ebee1e9a1cd078b79f36d2e
Jack Urbanek <jju@fb.com>
2020-06-24 17:08:01 -0400

    wrong name

--- mephisto/server/architects/heroku_architect.py
+++ mephisto/server/architects/heroku_architect.py
@@ -232,7 +232,7 @@ class HerokuArchitect(Architect):
             if os.path.exists(tar_path):
                 os.remove(tar_path)
 
-        heroku_directory_name = os.path.dirname(
+        heroku_directory_name = os.path.basename(
             glob.glob(os.path.join(HEROKU_TMP_DIR, "heroku-cli-*"))[0]
         )
         heroku_directory_path = os.path.join(HEROKU_TMP_DIR, heroku_directory_name)
