61fc7b2b9e15dace2d630706c00ba38f3358d723
Sara Ebrahim <sara.elkafrawy@gmail.com>
2020-06-30 12:45:42 -0400

    adding tasks dynamically 1st attempt

--- mephisto/core/task_launcher.py
+++ mephisto/core/task_launcher.py
@@ -32,6 +32,7 @@ from mephisto.core.logger_core import get_logger
 logger = get_logger(name=__name__, verbose=True, level="info")
 
 UNIT_GENERATOR_WAIT_SECONDS = 10
+ASSIGNMENT_GENERATOR_WAIT_SECONDS = 0.5
 
 
 class TaskLauncher:
@@ -58,18 +59,32 @@ class TaskLauncher:
         self.max_num_concurrent_units = max_num_concurrent_units
         self.launched_units: Dict[str, Unit] = {}
         self.unlaunched_units: Dict[str, Unit] = {}
-        self.keep_launching: bool = False
+        self.keep_launching_units: bool = False
+        self.assignment_exist: bool = False
         run_dir = task_run.get_run_dir()
         os.makedirs(run_dir, exist_ok=True)
 
-    def create_assignments(self) -> None:
-        """
-        Create an assignment and associated units for any data
-        currently in the assignment config
-        """
-        task_run = self.task_run
-        task_config = task_run.get_task_config()
+    def start(self, url):
+        """ Start generating assignments and units """
+        assignments_thread = threading.Thread(
+            target=self._try_create_assignments, args=()
+        )
+        assignments_thread.start()
+
+        units_thread = threading.Thread(target=self._try_launch_units, args=(url,))
+        units_thread.start()
+
+    def generate_assignments(self):
+        """ Generate an assignment data from the current dynamic self.assignment_data_list """
         for data in self.assignment_data_list:
+            self.assignment_exist = True
+            yield data
+
+    def _try_create_assignments(self) -> None:
+        """ Create an assignment and associated units for the generated assignment data """
+        for data in self.generate_assignments():
+            task_run = self.task_run
+            task_config = task_run.get_task_config()
             assignment_id = self.db.new_assignment(
                 task_run.task_id,
                 task_run.db_id,
@@ -96,12 +111,14 @@ class TaskLauncher:
                 )
                 self.units.append(Unit(self.db, unit_id))
                 self.unlaunched_units[unit_id] = Unit(self.db, unit_id)
-                self.keep_launching = True
+                self.keep_launching_units = True
+            time.sleep(ASSIGNMENT_GENERATOR_WAIT_SECONDS)
+        self.assignment_exist = False
 
     def generate_units(self):
         """ units generator which checks that only 'max_num_concurrent_units' running at the same time,
         i.e. in the LAUNCHED or ASSIGNED states """
-        while self.keep_launching:
+        while self.keep_launching_units:
             units_id_to_remove = []
             for db_id, unit in self.launched_units.items():
                 status = unit.get_status()
@@ -130,23 +147,21 @@ class TaskLauncher:
             if not self.unlaunched_units:
                 break
 
-    def _launch_limited_units(self, url: str) -> None:
-        """ use units' generator to launch limited number of units according to (max_num_concurrent_units)"""
-        for unit in self.generate_units():
-            unit.launch(url)
-
-    def launch_units(self, url: str) -> None:
-        """launch any units registered by this TaskLauncher"""
-        if self.max_num_concurrent_units > 0:
-            thread = threading.Thread(target=self._launch_limited_units, args=(url,))
-            thread.start()
-        else:
-            for db_id, unit in self.unlaunched_units.items():
-                unit.launch(url)
+    def _try_launch_units(self, url: str) -> None:
+        """ Launch all units or limited number of units according to (max_num_concurrent_units)"""
+        while self.assignment_exist:
+            if self.max_num_concurrent_units > 0:
+                for unit in self.generate_units():
+                    unit.launch(url)
+            else:
+                for _ in range(len(self.unlaunched_units.items())):
+                    db_id, unit = self.unlaunched_units.popitem()
+                    unit.launch(url)
+                time.sleep(0.5)
 
     def expire_units(self) -> None:
         """Clean up all units on this TaskLauncher"""
-        self.keep_launching = False
+        self.keep_launching_units = False
         for unit in self.units:
             try:
                 unit.expire()
