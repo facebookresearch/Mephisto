b99a09bbe0230f7c0a78188647bcb6df57fb9045
Pratik Ringshia <tikir@fb.com>
2020-10-06 14:37:37 -0400

    Create package mephisto-review-hook that exports useMephistoReview

---
+++ packages/mephisto-review-hook/src/index.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+
+window.ReactLib = React;
+
+function useMephistoReview() {
+  const [data, setData] = useState(null);
+  const [counter, setCounter] = useState(0);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("/data_for_current_task")
+      .catch((err) => {
+        setError({ type: "DATA_RETRIEVAL", error: err });
+      })
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => {
+        setError({ type: "DATA_PARSE", error: err });
+      });
+  }, [counter]);
+
+  const submitData = React.useCallback(
+    (data) => {
+      fetch("/submit_current_task", {
+        method: "POST",
+        body: JSON.stringify(data),
+      })
+        .catch((err) => {
+          setError({ type: "RESPONSE_SUBMIT", error: err });
+        })
+        .then(() => setCounter(counter + 1));
+    },
+    [counter, setCounter]
+  );
+
+  return {
+    isLoading: !data,
+    data: data && data.data,
+    isFinished: data && data.finished,
+    submit: submitData,
+    error: error,
+  };
+}
+
+export { useMephistoReview };
