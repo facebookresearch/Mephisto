c6b6021c631650431efbb8638049170c372a3dd6
Jack Urbanek <jju@fb.com>
2020-10-21 21:59:09 -0400

    Javascript Dependency update script (#286)
    * Auto updating some vulnerabilities
    
    * Update script to catch yarn locks too, and cleanup

---
+++ scripts/README.md
@@ -0,0 +1,9 @@
+# Mephisto repo scripts
+
+This directory contains helper scripts used for the purpose of maintaining the Mephisto repo.
+
+If you're looking for python scripts for using Mephisto, you should check the [mephisto/scripts](`https://github.com/facebookresearch/Mephisto/blob/master/mephisto/scripts`) directory.
+
+## UpdateJSDeps
+
+Recursively goes through all of the directories searching for js packages, and resolves potential security vulnerabilities. `./update_js_deps.sh dry-run` calls this without actually making the changes.
---
+++ scripts/update_js_deps.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Until dependabot seems to be running, this script will automatically update any
+# dependencies that appear to be vulnerabilities across our javascript packages
+
+if [ $# == 0 ]; then
+  DRY_RUN=0
+else
+  if [ "$1" = "dry-run" ]; then
+    DRY_RUN=1
+  else
+    echo "The only allowed argument to this script is dry-run"
+    exit 1
+  fi
+fi
+
+POSSIBLE_DIRS=`find .. -type d \( -name node_modules -o -name tmp -o -name runs -o -name _generated \) -prune -false -o -name 'package.json' -exec dirname {} \;`
+
+for d in $POSSIBLE_DIRS
+do
+  if [ $DRY_RUN = 1 ]; then
+    (cd "$d" && echo "Checking directory $d" && npm audit)
+  else
+    (cd "$d" && echo "Updating packages for directory $d" && npm install && npm audit fix && yarn install)
+  fi
+done
+
+# If the user has updated things, we'll want to add them to the commit only if we're modifying
+# something that already existed. Everything else should be removed
+
+if [ $DRY_RUN = 1 ]; then
+  exit 0
+else
+  git ls-files .. --others --exclude-standard | grep -E "\yarn.lock$" | xargs rm
+  git ls-files .. --others --exclude-standard | grep -E "\package-lock.json$" | xargs rm
+fi
