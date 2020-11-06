1124a64f0f70eb32bb1a2545512bac56febf379f
Jack Urbanek <jju@fb.com>
2020-11-06 12:25:44 -0500

    Fixing quickstart (#308)
    * moving around the stuff from data_model and webapp/client
    
    * Migrating crowd providers
    
    * Moving script utils
    
    * Migrating architects
    
    * Forgot to update channel callsites
    
    * Moving blueprints
    
    * Migrating core
    
    * merging 298
    
    * doc path refactor
    
    * Fixing some issues with the quickstart
    
    * Only one lock file

---
+++ mephisto/abstractions/blueprints/static_html_task/source/webpack.config.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  entry: "./dev/main.js",
+  output: {
+    path: __dirname,
+    filename: "build/bundle.js",
+  },
+  resolve: {
+    alias: {
+      react: path.resolve("./node_modules/react"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader",
+      },
+      {
+        test: /\.(svg|png|jpe?g|ttf)$/,
+        loader: "url-loader?limit=100000",
+      },
+      {
+        test: /\.jpg$/,
+        loader: "file-loader",
+      },
+    ],
+  },
+};
