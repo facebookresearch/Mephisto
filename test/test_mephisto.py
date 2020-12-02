5a34ff1df96e4d7fbabce4f72194533d83182103
Jack Urbanek <jju@fb.com>
2020-12-02 14:40:21 -0500

    Using VERSION file to prep versioning (#324)
    * Using VERSION file to prep versioning
    
    * Forgot to update version test

--- test/test_mephisto.py
+++ test/test_mephisto.py
@@ -5,7 +5,11 @@
 # LICENSE file in the root directory of this source tree.
 
 from mephisto import __version__
+from mephisto.operations.utils import get_root_dir
+import os
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    with open(os.path.join(get_root_dir(), "mephisto", "VERSION")) as version_file:
+        version = version_file.read().strip()
+    assert __version__ == version
