c9f902924d65045ddcac42be3e90aa93fcd0ec2a
Jing Xu <jingxu23@fb.com>
2021-01-25 20:37:01 +0000

    comment for next line

--- mephisto/operations/supervisor.py
+++ mephisto/operations/supervisor.py
@@ -805,7 +805,7 @@ class Supervisor:
                 if status != AgentState.STATUS_DISCONNECT:
                     # Stale or reconnect, send a status update
                     self._send_status_update(self.agents[agent_id])
-                    continue  # Only DISCONNECT can be marked remotely, rest are mismatch
+                    continue  # Only DISCONNECT can be marked remotely, rest are mismatch (except STATUS_COMPLETED)
                 agent.update_status(status)
         pass
 
