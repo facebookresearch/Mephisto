24ccd8fea0e61c0f74fdbe25ca8b1a4b6f1d57ce
pringshia <pratik@ringshia.com>
2021-03-19 16:14:50 -0400

    Streamline layers

--- packages/annotation-ui/src/Task.js
+++ packages/annotation-ui/src/Task.js
@@ -5,38 +5,34 @@ import BBoxFrame from "./layers/VQA/BBoxFrame";
 
 function Layers() {
   return (
-    <div>
-      <div className="bp3-tree">
-        <ul className="bp3-tree-node-list bp3-tree-root">
-          <Layer
-            name="Video"
-            icon="video"
-            component={VideoPlayer}
-            alwaysOn={true}
-          />
-          <Layer name="Query 1">
-            <Layer
-              name="Item Crop"
-              icon="widget"
-              component={BBoxFrame}
-              noPointerEvents={true}
-            />
-            <Layer name="Response Track" icon="path-search" />
-            <Layer name="Query Frame" icon="help" />
-          </Layer>
-          <Layer name="Query 2">
-            <Layer name="Item Crop" icon="widget" />
-            <Layer name="Response Track" icon="path-search" />
-            <Layer name="Query Frame" icon="help" />
-          </Layer>
-          <Layer name="Query 3">
-            <Layer name="Item Crop" icon="widget" />
-            <Layer name="Response Track" icon="path-search" />
-            <Layer name="Query Frame" icon="help" />
-          </Layer>
-        </ul>
-      </div>
-    </div>
+    <>
+      <Layer
+        name="Video"
+        icon="video"
+        component={VideoPlayer}
+        alwaysOn={true}
+      />
+      <Layer name="Query 1">
+        <Layer
+          name="Item Crop"
+          icon="widget"
+          component={BBoxFrame}
+          noPointerEvents={true}
+        />
+        <Layer name="Response Track" icon="path-search" />
+        <Layer name="Query Frame" icon="help" />
+      </Layer>
+      <Layer name="Query 2">
+        <Layer name="Item Crop" icon="widget" />
+        <Layer name="Response Track" icon="path-search" />
+        <Layer name="Query Frame" icon="help" />
+      </Layer>
+      <Layer name="Query 3">
+        <Layer name="Item Crop" icon="widget" />
+        <Layer name="Response Track" icon="path-search" />
+        <Layer name="Query Frame" icon="help" />
+      </Layer>
+    </>
   );
 }
 
--- packages/annotation-ui/src/panels/LayersPanel.js
+++ packages/annotation-ui/src/panels/LayersPanel.js
@@ -7,7 +7,11 @@ export default function ContextPanel() {
   return (
     <div style={{ display: "flex", height: "100%", flexDirection: "column" }}>
       <div style={{ flex: 1 }}>
-        <Layers />
+        <div className="bp3-tree">
+          <ul className="bp3-tree-node-list bp3-tree-root">
+            <Layers />
+          </ul>
+        </div>
       </div>
       <DebugPanel />
     </div>
