229dae42d71ca9b5f1bb511d870c55cde3845095
Sath Katugaha <sathira@fb.com>
2021-03-27 23:21:15 -0400

    Added instructional comments for future development purposes

--- test/integration/review/tests/test_constants.js
+++ test/integration/review/tests/test_constants.js
@@ -1,3 +1,8 @@
+/**
+ * Constant properties common to all tests of a given test type, certain attributes are mandatory for functionality of tests
+ */
+
+//get all test types
 import {
   CSV_MULTI_PAGE_DATA,
   CSV_SINGLE_PAGE_DATA,
@@ -5,10 +10,13 @@ import {
   JSON_SINGLE_PAGE_DATA,
 } from "./test_types";
 
+//path to python binary used by tests
 const PYTHON_PATH = "../../../venv/bin/python3";
 
 export default {
+  //match test type string identifier to key of test constant attribute
   [CSV_MULTI_PAGE_DATA]: {
+    //MANDATORY for all tests, arguments for Nodejs spawn process to start mephisto review server
     SERVER_START_ARGS: [
       PYTHON_PATH,
       [
@@ -19,15 +27,24 @@ export default {
         "--all",
       ],
     ],
+    //used to create Nodejs spawn process to load standard input of data points into mephisto review server
     DATA_COLLECTION_ARGS: ["cat", ["test_data/CSV/multi_page_data.csv"]],
+    //used to check if review server started properly, is supposed to match server output upon successful start
     DEFAULT_SERVER_OUTPUT:
       "Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)\n",
+    //type of data being used to run mephisto review server
     TYPE: "CSV",
+    //MANDATORY for item_view_ui and review_results tests amount of pages of data review app will display during test
     DATA_PAGE_COUNT: 2,
+    //MANDATORY for item_view_ui and review_results tests number of data points review app will show on each page during test
     ITEMS_PER_PAGE: 9,
+    //MANDATORY for search portion of all_item_view_ui test
     SEARCH_TEST_OPTIONS: {
+      //MANDATORY, represents the query used on review app search bar
       QUERY: "bad",
+      //expected amount of data points to match query
       EXPECTED_RESULT_COUNT: 6,
+      //expected number of pages of data points review app will show upon query
       RESULT_DATA_PAGE_COUNT: 1,
     },
   },
@@ -102,11 +119,17 @@ export default {
       RESULT_DATA_PAGE_COUNT: 1,
     },
   },
+  //MANDATORY for all tests, represents information common to all test types
   ALL: {
+    //time to wait for server to start successfully
     SERVER_START_DELAY: 1000,
+    //time to wait for server to stop successfully
     SERVER_STOP_DELAY: 500,
+    //root URL of review app
     BASE_URL: "http://127.0.0.1:5000/",
+    //result review app will record upon rejecting a review
     REJECTED_REVIEW: "REVIEW: {'result': 'rejected'}",
+    //result review app will record upon approving a review
     APPROVED_REVIEW: "REVIEW: {'result': 'approved'}",
   },
 };
--- test/integration/review/utilities/test_tools.js
+++ test/integration/review/utilities/test_tools.js
@@ -1,3 +1,11 @@
+/**
+ * Collection of methods to act as tools tests can use
+ */
+
+/**
+ * Delays program for specified time in milliseconds. Must be used in async mode.
+ * @param {Number} time time in milliseconds to delay process.
+ */
 export const delay = (time) => {
   return new Promise((resolve) => {
     setTimeout(() => {
