f42420636ad4f2868ef9196ed3c6c2af8e8e1984
Jack Urbanek <jju@fb.com>
2021-07-08 17:35:15 -0400

    Loading from local profiles too

--- hydra_plugins/mephisto_path_plugin.py
+++ hydra_plugins/mephisto_path_plugin.py
@@ -7,19 +7,18 @@
 from hydra.core.config_search_path import ConfigSearchPath
 from hydra.plugins.search_path_plugin import SearchPathPlugin
 
-from mephisto.core.utils import get_root_dir
+from mephisto.operations.utils import get_root_dir
+from mephisto.operations.config_handler import DEFAULT_CONFIG_FOLDER
 import os
 
 
 class MephistoSearchPathPlugin(SearchPathPlugin):
     def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
         # Appends the search path for this plugin to the end of the search path
-        # Note that foobar/conf is outside of the example plugin module.
-        # There is no requirement for it to be packaged with the plugin, it just needs
-        # be available in a package.
-        # Remember to verify the config is packaged properly (build sdist and look inside,
-        # and verify MANIFEST.in is correct).
-
         profile_path = os.path.join(get_root_dir(), "hydra_configs")
+        profile_path = os.path.join(DEFAULT_CONFIG_FOLDER, "hydra_configs")
 
         search_path.append(provider="mephisto-profiles", path=f"file://{profile_path}")
+        search_path.append(
+            provider="mephisto-profiles-user", path=f"file://{profile_path}"
+        )
