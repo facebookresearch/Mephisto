b9a9bd49968d7f6bac3bd60bb20c98263d0178ca
Pratik <tikir@fb.com>
2021-08-17 22:35:30 +0200

    Enable symlinking js bundle via Hydra prop to make for easier local development for static_react_task (#509)
    * Add mephisto.blueprint.link_task_source property
    
    * Update readme
    
    * Update help metadata
    
    * Add args validation in assert_task_args

--- mephisto/abstractions/blueprints/static_react_task/static_react_blueprint.py
+++ mephisto/abstractions/blueprints/static_react_task/static_react_blueprint.py
@@ -57,6 +57,18 @@ class StaticReactBlueprintArgs(StaticBlueprintArgs):
             "required": True,
         },
     )
+    link_task_source: bool = field(
+        default=False,
+        metadata={
+            "help": """
+                Symlinks the task_source file in your development folder to the
+                one used for the server. Useful for local development so you can run
+                a watch-based build for your task_source, allowing the UI code to
+                update without having to restart the server each time.
+            """,
+            "required": False,
+        },
+    )
 
 
 @register_mephisto_abstraction()
@@ -88,7 +100,15 @@ class StaticReactBlueprint(StaticBlueprint):
         assert (
             found_task_source is not None
         ), "Must provide a path to a javascript bundle in `task_source`"
+
         found_task_path = os.path.expanduser(found_task_source)
         assert os.path.exists(
             found_task_path
         ), f"Provided task source {found_task_path} does not exist."
+
+        link_task_source = args.blueprint.link_task_source
+        current_architect = args.architect._architect_type
+        allowed_architects = ["local"]
+        assert link_task_source == False or (
+            link_task_source == True and current_architect in allowed_architects
+        ), f"`link_task_source={link_task_source}` is not compatible with architect type: {args.architect._architect_type}. Please check your task configuration."
