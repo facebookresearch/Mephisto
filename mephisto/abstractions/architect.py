32ebf7e811f5f88413c84c7b5383f0ae29c5521b
pringshia <pratik@ringshia.com>
2021-09-09 12:54:14 -0400

    Find replace fix typo

--- mephisto/abstractions/architect.py
+++ mephisto/abstractions/architect.py
@@ -11,7 +11,7 @@ from typing import Dict, List, Any, ClassVar, Type, TYPE_CHECKING, Callable
 
 if TYPE_CHECKING:
     from mephisto.abstractions.channel import Channel
-    from mephsito.data_model.packet import Packet
+    from mephisto.data_model.packet import Packet
     from mephisto.data_model.task_run import TaskRun
     from mephisto.abstractions.database import MephistoDB
     from mephisto.abstractions.blueprint import SharedTaskState
