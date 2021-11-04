5ff75d036193964fb619aacf29982e3d70072a3b
pringshia <pratik@ringshia.com>
2021-11-03 23:59:54 -0400

    Change host origin bug and rename package name

--- packages/cra-template-mephisto-review/template/src/utils.js
+++ packages/cra-template-mephisto-review/template/src/utils.js
@@ -3,7 +3,7 @@ import config from "./config";
 function getHostname() {
   return config.port
     ? `${window.location.protocol}//${window.location.hostname}:${config.port}`
-    : window.location.host;
+    : window.location.origin;
 }
 
 export { getHostname };
