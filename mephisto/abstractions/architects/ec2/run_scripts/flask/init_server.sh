471b36057b41b4c4de0cde671552bf5652d5cd2d
Jack Urbanek <jju@fb.com>
2021-11-11 14:58:25 -0800

    Cleaning up scripts

--- mephisto/abstractions/architects/ec2/run_scripts/flask/init_server.sh
+++ mephisto/abstractions/architects/ec2/run_scripts/flask/init_server.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo yum install -y httpd
+echo "Installing requirements"
+sudo yum update -y >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
+sudo yum install -y httpd >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
 
-sudo -H pip3 install flask gevent
+sudo -H pip3 install flask gevent >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
 
+echo "Preparing service..."
 sudo cp /home/ec2-user/routing_server/setup/router.service /etc/systemd/system/router.service
 sudo chmod 744 /home/ec2-user/routing_server/setup/run_server.sh
 sudo chmod 664 /etc/systemd/system/router.service
 
-sudo systemctl daemon-reload
-sudo systemctl enable router.service
-sudo systemctl start router.service
+echo "Launching service..."
+sudo systemctl daemon-reload >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
+sudo systemctl enable router.service >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
+sudo systemctl start router.service >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
--- mephisto/abstractions/architects/ec2/run_scripts/node/init_server.sh
+++ mephisto/abstractions/architects/ec2/run_scripts/node/init_server.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo yum install -y httpd
+echo "Installing basic requirements..."
+sudo yum update -y >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
+sudo yum install -y httpd >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+echo "Downloading Node..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
+. ~/.nvm/nvm.sh >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
+nvm install node >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
 
+echo "Installing router modules..."
 cd /home/ec2-user/routing_server/router/
-npm install
+npm install >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
 
+echo "Preparing service..."
 sudo cp /home/ec2-user/routing_server/setup/router.service /etc/systemd/system/router.service
 sudo chmod 744 /home/ec2-user/routing_server/setup/run_server.sh
 sudo chmod 664 /etc/systemd/system/router.service
 
-sudo systemctl daemon-reload
-sudo systemctl enable router.service
-sudo systemctl start router.service
+echo "Launching service..."
+sudo systemctl daemon-reload >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
+sudo systemctl enable router.service >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
+sudo systemctl start router.service >> /home/ec2-user/routing_server/setup/setup_log.txt 2>&1
