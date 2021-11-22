457fb79cd65880ed3c93505b58a186748f14e29c
pringshia <pratik@ringshia.com>
2021-11-22 16:50:51 -0500

    Update broken link

--- docs/web/src/pages/index.js
+++ docs/web/src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/quickstart"
+            to="/docs/guides/quickstart"
           >
             Mephisto Quickstart - 10min ⏱️
           </Link>
