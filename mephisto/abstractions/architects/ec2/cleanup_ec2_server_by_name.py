1e1db28849391ee4338b4677f899434470514e23
Jing Xu <jingxu23@fb.com>
2022-01-08 20:54:32 -0800

    typo

--- mephisto/abstractions/architects/ec2/cleanup_ec2_server_by_name.py
+++ mephisto/abstractions/architects/ec2/cleanup_ec2_server_by_name.py
@@ -19,7 +19,6 @@ logger = get_logger(name=__name__)
 
 # TODO Hydrize
 def main():
-    iam_role_name = input("Please enter local profile name for IAM role\n>> ")
     all_server_names = [
         os.path.splitext(s)[0]
         for s in os.listdir(DEFAULT_SERVER_DETAIL_LOCATION)
@@ -37,6 +36,8 @@ def main():
         os.path.join(DEFAULT_SERVER_DETAIL_LOCATION, f"{server_name}.json")
         != DEFAULT_FALLBACK_FILE
     ), "This is going to completely delete the fallback server for your EC2 architect."
+
+    iam_role_name = input("Please enter local profile name for IAM role\n>> ")
 
     session = boto3.Session(profile_name=iam_role_name, region_name="us-east-2")
     ec2_helpers.remove_instance_and_cleanup(session, server_name)
