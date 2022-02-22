86cfd51a098727a122b8018d1ceca84e2601d8e4
pringshia <pratik@ringshia.com>
2022-02-22 09:17:01 -0500

    Fix script typo and parameterize package checks

--- scripts/check_npm_package_versions.py
+++ scripts/check_npm_package_versions.py
@@ -18,38 +18,45 @@ from mephisto.operations.logger_core import format_loud
 
 
 ROOT_DIR = get_root_dir()
-MEPHISTO_TASK_PACKAGE = os.path.join(ROOT_DIR, "packages/mephisto-task/package.json")
+CHECK_PACKAGES = ["mephisto-task", "bootstrap-chat"]
 
 
 def run_check():
-    assert os.path.exists(
-        MEPHISTO_TASK_PACKAGE
-    ), f"Can't find task package at {MEPHISTO_TASK_PACKAGE}"
-    with open(MEPHISTO_TASK_PACKAGE) as mephisto_task_package:
-        version = json.load(mephisto_task_package)["version"]
+    all_success = True
+    for pkg in CHECK_PACKAGES:
+        package_location = os.path.join(ROOT_DIR, "packages", pkg, "package.json")
+        assert os.path.exists(
+            package_location
+        ), f"Can't find package {pkg} at {package_location}"
+        with open(package_location) as package_json:
+            version = json.load(package_json)["version"]
 
-    print(f"Detected mephisto-task version '{version}' at '{MEPHISTO_TASK_PACKAGE}'")
+        print()
+        print(f"Detected '{pkg}' version '{version}' at '{package_location}'")
 
-    link = "https://registry.npmjs.org/-/package/mephisto-task/dist-tags"
+        link = f"https://registry.npmjs.org/-/package/{pkg}/dist-tags"
 
-    try:
-        f = urlopen(link)
-        contents = f.read().decode("utf-8")
-        latest_version = json.loads(contents)["latest"]
-        print(f"Detected mephisto-task@latest as version '{version}' on npm")
-    except:
-        print(
-            f"{format_loud('[ERROR]')} Could not access latest version of package on npm."
-        )
-        sys.exit(1)
+        try:
+            f = urlopen(link)
+            contents = f.read().decode("utf-8")
+            latest_version = json.loads(contents)["latest"]
+            print(f"Detected '{pkg}@latest' as version '{latest_version}' on npm")
+        except:
+            print(
+                f"{format_loud('[ERROR]')} Could not access latest version of package '{pkg}' on npm."
+            )
+            all_success &= False
 
-    if latest_version != version:
-        print(
-            f"{format_loud('[VERSION MISMATCH]')} The latest version of the "
-            f"'mephisto-task' package on npm ({latest_version}) doesn't match "
-            f"the version in the codebase ({version}). If this is part of a"
-            f"merge onto the main branch, you may want to deploy the packages first."
-        )
+        if latest_version != version:
+            print(
+                f"{format_loud('[VERSION MISMATCH]')} The latest version of the "
+                f"'{pkg}' package on npm ({latest_version}) doesn't match "
+                f"the version in the codebase ({version}). If this is part of a"
+                f"merge onto the main branch, you may want to deploy the packages first."
+            )
+            all_success &= False
+
+    if not all_success:
         sys.exit(1)
 
 
