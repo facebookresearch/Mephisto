da0d2fb6bd3a591b206bf211a98eb635534f088a
Jack Urbanek <jju@fb.com>
2022-03-09 18:16:45 -0500

    removing unused imports

--- mephisto/operations/hydra_config.py
+++ mephisto/operations/hydra_config.py
@@ -4,8 +4,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import functools
-import hydra
 from hydra.core.config_store import ConfigStoreWithProvider
 from mephisto.abstractions.blueprint import BlueprintArgs
 from mephisto.abstractions.architect import ArchitectArgs
