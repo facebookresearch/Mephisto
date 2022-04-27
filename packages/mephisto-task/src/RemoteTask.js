12b3bdafc263770394efc96bec22c82775d1bd67
Jack Urbanek <jju@fb.com>
2022-04-27 07:52:21 -0700

    Fixing agent desync on RemoteProcedure tasks

--- packages/mephisto-task/src/RemoteTask.js
+++ packages/mephisto-task/src/RemoteTask.js
@@ -27,6 +27,7 @@ const useMephistoRemoteProcedureTask = function (props) {
   // mapping of request ID to the callback object
   const requestCallbacks = {};
   const [disconnectIssueText, setDisconnectIssueText] = React.useState();
+  const [connectedAgentId, setConnectedAgentId] = React.useState();
 
   // We register a ref so that the liveUpdate handler has access
   // to the current set of callbacks
@@ -68,9 +69,9 @@ const useMephistoRemoteProcedureTask = function (props) {
     connect,
     destroy,
     sendLiveUpdate,
+    agentId,
     ...otherMephistoProps
   } = mephistoProps;
-  let { agentId } = mephistoProps;
 
   // They're still exposed in a separate manner though
   const _fullSocketProps = {
@@ -83,6 +84,7 @@ const useMephistoRemoteProcedureTask = function (props) {
     if (agentId) {
       console.log("connecting...");
       connect(agentId);
+      setConnectedAgentId(agentId);
     }
   }, [agentId]);
 
@@ -131,6 +133,7 @@ const useMephistoRemoteProcedureTask = function (props) {
 
   return {
     ...otherMephistoProps,
+    agentId: connectedAgentId, // only show connected agent Ids
     remoteProcedure,
     disconnectIssueText,
     _fullSocketProps,
