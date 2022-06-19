c9448b6755b47ce3a917be8fc57501dba86bd42d
Etesam <etesamansari13@gmail.com>
2022-06-19 14:23:30 -0400

    Create review_tips_for_task.py

---
+++ mephisto/scripts/local_db/review_tips_for_task.py
@@ -0,0 +1,25 @@
+def main():
+    db = LocalMephistoDB()
+    mephisto_data_browser = MephistoDataBrowser(db)
+    task_names = mephisto_data_browser.get_task_name_list()
+    acceptable_responses = set(
+        ["a", "accept", "ACCEPT", "Accept", "r", "reject", "REJECT", "Reject"]
+    )
+    accept_response = set(["a", "accept", "ACCEPT", "Accept"])
+    reject_response = set(["r", "reject", "REJECT", "Reject"])
+    yes_no_responses = set(["yes", "y", "YES", "Yes", "no", "n", "NO", "No"])
+    yes_response = set(["yes", "y", "YES", "Yes"])
+    no_response = set(["no", "n", "NO", "No"])
+
+    print("\nTask Names:")
+    for task_name in task_names:
+        print(task_name)
+    print("")
+    task_name = input(
+        "Enter the name of the task that you want to review the tips of: \n"
+    )
+    print("")
+    
+    
+if __name__ == "__main__":
+  main()
