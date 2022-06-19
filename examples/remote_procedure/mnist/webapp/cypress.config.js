82c7f31c45eedbb70a48a6d6b9709674e5ad7b11
Etesam Ansari <etesam1@fb.com>
2022-06-19 00:35:47 -0400

    ✅ Added tests for remote_procedure/mnist

---
+++ examples/remote_procedure/mnist/webapp/cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  video: false,
+
+  e2e: {
+    baseUrl: "http://localhost:3000/?worker_id=x&assignment_id=1",
+    supportFile: false,
+    experimentalSourceRewriting: true,
+    /* setupNodeEvents(on, config) {
+      // implement node event listeners here
+    }, */
+  },
+});
