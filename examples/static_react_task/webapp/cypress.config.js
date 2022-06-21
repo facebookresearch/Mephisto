f90408924982b0de69fde8f0a6d87bf01e8720b9
Etesam Ansari <etesam1@fb.com>
2022-06-20 20:52:23 -0400

    🔥 Removed defineConfig from cypressConfig

--- examples/static_react_task/webapp/cypress.config.js
+++ examples/static_react_task/webapp/cypress.config.js
@@ -1,12 +1,9 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
+module.exports = {
   video: false,
-
   e2e: {
     baseUrl: "http://localhost:3000/?worker_id=x&assignment_id=1",
     /* setupNodeEvents(on, config) {
       // implement node event listeners here
     }, */
   },
-});
+};
