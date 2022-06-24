041ac38b8012ea399a1fd2989066b8bef1c71084
Etesam Ansari <etesam1@fb.com>
2022-06-24 16:17:49 -0400

    🎨 Increased indentation of expire_all_units.py

--- mephisto/scripts/local_db/expire_all_units.py
+++ mephisto/scripts/local_db/expire_all_units.py
@@ -13,11 +13,11 @@ def main():
     assert len(tasks) >= 1, f"No task found under name {task_name}"
     task_runs = db.find_task_runs(task_id=tasks[0].db_id)
     for task_run in task_runs:
-      assignments = task_run.get_assignments()
-      for assignment in assignments:
-        found_units = assignment.get_units()
-        for unit in found_units:
-          unit.set_db_status("expired")
+        assignments = task_run.get_assignments()
+        for assignment in assignments:
+            found_units = assignment.get_units()
+            for unit in found_units:
+                unit.set_db_status("expired")
     
 if __name__ == "__main__":
     main()
