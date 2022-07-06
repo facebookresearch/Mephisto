cd3a36c54e79cecc22e54980a86db53810b6953b
Etesam Ansari <etesam1@fb.com>
2022-07-06 10:39:59 -0400

    🚨 Removed parameter warnings as they were too noisy
    🚨 Added an exception when the post_install_script does not execute correctly
    
    💡 Added a comment that has a link to a GitHub issue that explains how to set up force_rebuild and post_install_script

--- mephisto/tools/scripts.py
+++ mephisto/tools/scripts.py
@@ -224,13 +224,23 @@ def augment_config_from_db(script_cfg: DictConfig, db: "MephistoDB") -> DictConf
     return script_cfg
 
 
-def build_custom_bundle(custom_src_dir, **kwargs: Union[str, bool]):
+def build_custom_bundle(
+    custom_src_dir,
+    force_rebuild: Optional[bool] = None,
+    post_install_script: Optional[str] = None,
+):
     """Locate all of the custom files used for a custom build, create
     a prebuild directory containing all of them, then build the
     custom source.
 
     Check dates to only go through this build process when files have changes
     """
+
+    """
+    If doing local package development make sure to check out the below link:
+    https://github.com/facebookresearch/Mephisto/issues/811
+    """
+
     IGNORE_FOLDERS = {"node_modules", "build"}
 
     prebuild_path = os.path.join(custom_src_dir, "webapp")
@@ -238,20 +248,8 @@ def build_custom_bundle(custom_src_dir, **kwargs: Union[str, bool]):
     IGNORE_FOLDERS = {os.path.join(prebuild_path, f) for f in IGNORE_FOLDERS}
     build_path = os.path.join(prebuild_path, "build", "bundle.js")
 
-    if "force_rebuild" not in kwargs:
-        warnings.warn(
-            "Consider adding the force_rebuild property to your hydra config and passing it into build_custom_bundle()\nSteps on how to do this: \nhttps://github.com/facebookresearch/Mephisto/issues/811",
-            stacklevel=2,
-        )
-
-    if "post_install_script" not in kwargs:
-        warnings.warn(
-            "Consider adding the post_install_script property to your hydra config and passing it into build_custom_bundle()\nSteps on how to do this: \nhttps://github.com/facebookresearch/Mephisto/issues/811",
-            stacklevel=2,
-        )
-
     # see if we need to rebuild
-    if "force_rebuild" not in kwargs or kwargs["force_rebuild"] == False:
+    if force_rebuild is None or force_rebuild == False:
         if os.path.exists(build_path):
             created_date = os.path.getmtime(build_path)
             up_to_date = True
@@ -284,14 +282,14 @@ def build_custom_bundle(custom_src_dir, **kwargs: Union[str, bool]):
             "the above error for more info."
         )
 
-    if (
-        "post_install_script" in kwargs
-        and kwargs["post_install_script"] is not None
-        and isinstance(kwargs["post_install_script"], str)
-    ):
-        post_install_string: str = str(kwargs["post_install_script"])
-        if len(post_install_string) > 0:
-            subprocess.call(["bash", post_install_string])
+    if post_install_script is not None and len(post_install_script) > 0:
+        did_fail_script = subprocess.call(["bash", post_install_script])
+        if did_fail_script != 0:
+            raise Exception(
+                "Please make sure that the post_install_script mentioned in your hydra config "
+                "exists in the webapp folder for this task!\n"
+                "The script should be able to be ran with bash"
+            )
 
     webpack_complete = subprocess.call(["npm", "run", "dev"])
     if webpack_complete != 0:
