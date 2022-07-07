1b7aca963e00a02e9ee2f7bd00e66bd9b4d085a4
Etesam Ansari <etesam1@fb.com>
2022-07-07 11:44:17 -0400

    ♻️ Reduced the number of colors & used Prompt.ask

--- mephisto/scripts/local_db/remove_accepted_tip.py
+++ mephisto/scripts/local_db/remove_accepted_tip.py
@@ -3,8 +3,8 @@ from mephisto.scripts.local_db.review_tips_for_task import remove_tip_from_metad
 from mephisto.tools.data_browser import DataBrowser as MephistoDataBrowser
 from mephisto.utils.rich import console
 from rich import print
+from rich.prompt import Prompt
 from rich.markdown import Markdown
-from rich.prompt import Confirm
 from rich.table import Table, Column
 from rich import box
 
@@ -13,28 +13,19 @@ def main():
     db = LocalMephistoDB()
     mephisto_data_browser = MephistoDataBrowser(db)
     task_names = mephisto_data_browser.get_task_name_list()
-    task_names_text = """# Task Names:"""
+    task_names_text = """# Tips Removal\n ## Task Names:"""
     for task_name in task_names:
         task_names_text += "\n* " + task_name
 
     task_names_markdown = Markdown(task_names_text)
     console.print(task_names_markdown)
     print("")
-    task_name = Confirm.get_input(
-        console,
-        "[green]Enter the name of the task that you want to review the tips of[/green]: \n",
-        False,
+    task_name = Prompt.ask(
+        "\nEnter the name of the task that you want to review the tips of",
+        choices=task_names,
+        show_choices=False,
     ).strip()
 
-    print("")
-    while task_name not in task_names:
-        print("[red]That task name is not valid[/red]\n")
-        task_name = Confirm.get_input(
-            console,
-            "[green]Enter the name of the task that you want to review the tips of[/green]: \n",
-            False,
-        ).strip()
-        print("")
     units = mephisto_data_browser.get_all_units_for_task_name(task_name)
     if len(units) == 0:
         print("[red]No units were received[/red]")
@@ -46,12 +37,12 @@ def main():
             tips = metadata["tips"]
             accepted_tips = list(filter(lambda tip: tip["accepted"] == True, tips))
             accepted_tips_copy = accepted_tips.copy()
-            acceptable_removal_responses = set(["yes", "y", "no", "n"])
+
             for i in range(len(accepted_tips)):
                 current_tip_table = Table(
                     "Property",
-                    Column("Value", style="blue bold"),
-                    title="Current Tip",
+                    Column("Value"),
+                    title="Tip " + str(i + 1) + " of " + str(len(accepted_tips)),
                     box=box.ROUNDED,
                     expand=True,
                     show_lines=True,
@@ -61,26 +52,17 @@ def main():
                 current_tip_table.add_row("Tip Text", accepted_tips[i]["text"])
                 console.print(current_tip_table)
 
-                removal_response = Confirm.get_input(
-                    console,
-                    "\n[green]Do you want to remove this tip?[/green] [magenta]yes(y)/no(n)[/magenta]: \n",
-                    False,
+                removal_response = Prompt.ask(
+                    "\nDo you want to remove this tip", choices=["y", "n"]
                 ).strip()
                 print("")
-                while removal_response not in acceptable_removal_responses:
-                    print("[red]That response is not valid[/red]\n")
-                    removal_response = Confirm.get_input(
-                        console,
-                        "[green]Do you want to remove this tip?[/green] [magenta]yes(y)/no(n)[/magenta]: \n",
-                        False,
-                    ).strip()
-                    print("")
-                if removal_response == "y" or removal_response == "yes":
+
+                if removal_response == "y":
                     remove_tip_from_metadata(accepted_tips, accepted_tips_copy, i, unit)
-                    print("[green]Removed tip[/green]\n")
-                elif removal_response == "n" or removal_response == "no":
-                    print("[green]Did not remove tip[/green]\n")
-    print("[green]There are no more tips to look at[/green]\n")
+                    print("Removed tip\n")
+                elif removal_response == "n":
+                    print("Did not remove tip\n")
+    print("There are no more tips to look at\n")
 
 
 if __name__ == "__main__":
--- mephisto/scripts/local_db/review_tips_for_task.py
+++ mephisto/scripts/local_db/review_tips_for_task.py
@@ -17,8 +17,9 @@ from mephisto.tools.data_browser import DataBrowser as MephistoDataBrowser
 from rich import print
 from rich.markdown import Markdown
 from rich import box
+from rich.prompt import Prompt
 from rich.prompt import Confirm, FloatPrompt
-from rich.table import Table, Column
+from rich.table import Table
 from mephisto.utils.rich import console
 
 
@@ -58,30 +59,22 @@ def main():
     db = LocalMephistoDB()
     mephisto_data_browser = MephistoDataBrowser(db)
     task_names = mephisto_data_browser.get_task_name_list()
-    task_names_text = """# Task Names:"""
+    task_names_text = """# Tips Review \n ## Task Names:"""
     for task_name in task_names:
         task_names_text += "\n* " + task_name
 
     task_names_markdown = Markdown(task_names_text)
     console.print(task_names_markdown)
     print("")
-    task_name = Confirm.get_input(
-        console,
-        "[green]Enter the name of the task that you want to review the tips of[/green]: \n",
-        False,
+    task_name = Prompt.ask(
+        "\nEnter the name of the task that you want to review the tips of",
+        choices=task_names,
+        show_choices=False,
     ).strip()
     print("")
-    while task_name not in task_names:
-        print("[red]That task name is not valid\n[/red]")
-        task_name = Confirm.get_input(
-            console,
-            "[green]Enter the name of the task that you want to review the tips of[/green]: \n",
-            False,
-        ).strip()
-        print("")
     units = mephisto_data_browser.get_all_units_for_task_name(task_name)
     if len(units) == 0:
-        print("No units were received")
+        print("[red]No units were received[/red]")
         quit()
     for unit in units:
         if unit.agent_id is not None:
@@ -95,8 +88,8 @@ def main():
                     if tips[i]["accepted"] == False:
                         current_tip_table = Table(
                             "Property",
-                            Column("Value", style="blue bold"),
-                            title="Current Tip",
+                            "Value",
+                            title="Tip " + str(i + 1) + " of " + str(len(tips)),
                             box=box.ROUNDED,
                             expand=True,
                             show_lines=True,
@@ -106,49 +99,30 @@ def main():
                         current_tip_table.add_row("Tip Text", tips[i]["text"])
                         console.print(current_tip_table)
 
-                        tip_response = Confirm.get_input(
-                            console,
-                            "[green]Do you want to accept or reject this tip?[/green] [magenta]accept(a)/reject(r):[/magenta] \n",
-                            False,
+                        tip_response = Prompt.ask(
+                            "\nDo you want to accept, reject, or skip this tip",
+                            choices=["a", "r", "s"],
                         ).strip()
 
                         print("")
-                        acceptable_tip_responses = set(["a", "accept", "r", "reject"])
-                        while tip_response not in acceptable_tip_responses:
-                            print("[red]That response is not valid[/red]\n")
-                            tip_response = Confirm.get_input(
-                                console,
-                                "[green]Do you want to accept or reject this tip?[/green] [magenta]accept(a)/reject(r):[/magenta] \n",
-                                False,
-                            ).strip()
-                            print("")
-                        if tip_response == "a" or tip_response == "accept":
+                        if tip_response == "a":
                             # persists the tip in the db as it is accepted
                             accept_tip(tips, tips_copy, i, unit)
-                            print("[green]Tip Accepted[/green]\n")
+                            print("Tip Accepted")
                             # given the option to pay a bonus to the worker who wrote the tip
-                            is_bonus = Confirm.get_input(
-                                console,
-                                "[green]Do you want to pay a bonus to this worker for their tip?[/green] [magenta]yes(y)/no(n):[/magenta] \n",
-                                False,
+                            is_bonus = Prompt.ask(
+                                "\nDo you want to pay a bonus to this worker for their tip",
+                                choices=["y", "n"],
                             ).strip()
-                            acceptable_bonus_responses = set(["yes", "y", "no", "n"])
-                            while is_bonus not in acceptable_bonus_responses:
-                                print("[red]That response is not valid[/red]\n")
-                                is_bonus = Confirm.get_input(
-                                    console,
-                                    "[green]Do you want to pay a bonus to this worker for their tip?[/green] [magenta]yes(y)/no(n):[/magenta] \n",
-                                    False,
-                                ).strip()
-                                print("")
-                            if is_bonus == "y" or is_bonus == "yes":
+
+                            if is_bonus == "y":
                                 bonus_amount = FloatPrompt.ask(
-                                    "[green]How much money do you want to give[/green]"
+                                    "\nHow much money do you want to give"
                                 )
 
                                 reason = Confirm.get_input(
                                     console,
-                                    "\n[green]What is your reason for the bonus: [/green]\n",
+                                    "\nWhat is your reason for the bonus:",
                                     False,
                                 )
                                 worker_id = float(unit_data["worker_id"])
@@ -158,21 +132,21 @@ def main():
                                         bonus_amount, reason, unit
                                     )
                                     if bonus_successfully_paid:
-                                        print(
-                                            "\n[green]Bonus Successfully Paid![/green]\n"
-                                        )
+                                        print("\nBonus Successfully Paid!\n")
                                     else:
                                         print(
                                             "\n[red]There was an error when paying out your bonus[/red]\n"
                                         )
-                            elif is_bonus == "n" or is_bonus == "no":
-                                print("[green]No bonus paid[/green]\n")
+                            elif is_bonus == "n":
+                                print("No bonus paid\n")
 
-                        elif tip_response == "r" or tip_response == "reject":
+                        elif tip_response == "r":
                             remove_tip_from_metadata(tips, tips_copy, i, unit)
-                            print("[green]Tip Rejected[/green]\n")
+                            print("Tip Rejected\n")
+                        elif tip_response == "s":
+                            print("Tip Skipped\n")
 
-    print("[green]There are no more tips to review[/green]\n")
+    print("There are no more tips to review\n")
 
 
 if __name__ == "__main__":
