2d4a8b8dc8cf3159b82c2f59406b1fcb6b7f3750
Etesam Ansari <etesam1@fb.com>
2022-07-15 15:32:24 -0400

    ✅ Updated post_tip_submission test

--- examples/static_react_task_with_tips/webapp/cypress/e2e/post_submission_tests/static_react_task_with_tips_post_tip_submission.cy.js
+++ examples/static_react_task_with_tips/webapp/cypress/e2e/post_submission_tests/static_react_task_with_tips_post_tip_submission.cy.js
@@ -1,19 +1,33 @@
 import { tipClassNamePrefix } from "../../helper";
 describe("Checking for tips", () => {
+  // Checks if the two most recent tips are what was submitted in the pre_tip_submission test
   it("Checks for recently added tip", () => {
     cy.visit("/");
     cy.get(`.${tipClassNamePrefix}button`).as("tipsButton");
     cy.get("@tipsButton").click();
 
-    cy.get(`.${tipClassNamePrefix}tip`).last().as("lastTip");
+    cy.get(`.${tipClassNamePrefix}tip`).eq(-2).as("secondToLastTip");
+
+    cy.get("@secondToLastTip").find("h2").as("secondToLastTipHeader");
+    cy.get("@secondToLastTip").find("p").as("secondToLastTipBody");
+
+    cy.get("@secondToLastTipHeader").should(
+      "have.text",
+      "🎉 This is my test tip header"
+    );
+    cy.get("@secondToLastTipBody").should(
+      "have.text",
+      "🎈 This is my test tip body"
+    );
+    cy.get(`.${tipClassNamePrefix}tip`).eq(-1).as("lastTip");
+
     cy.get("@lastTip").find("h2").as("lastTipHeader");
     cy.get("@lastTip").find("p").as("lastTipBody");
 
     cy.get("@lastTipHeader").should(
       "have.text",
-      "🎉 This is my test tip header"
+      "This is my second tip header"
     );
-
-    cy.get("@lastTipBody").should("have.text", "🎈 This is my test tip body");
+    cy.get("@lastTipBody").should("have.text", "This is my second tip body");
   });
 });
