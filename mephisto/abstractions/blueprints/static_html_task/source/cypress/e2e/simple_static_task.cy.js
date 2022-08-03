94e3b697b9aa6b78b6b02578e639f1d042b96dc2
Etesam Ansari <etesam1@fb.com>
2022-08-03 11:04:20 -0400

    ✅ Created cypress tests for html_static+task

---
+++ mephisto/abstractions/blueprints/static_html_task/source/cypress/e2e/simple_static_task.cy.js
@@ -0,0 +1,50 @@
+describe("Loads simple_static_task", () => {
+  it("Makes request for agent", () => {
+    cy.intercept({ pathname: "/request_agent" }).as("agentRequest");
+    cy.visit("/");
+    cy.wait("@agentRequest").then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+    });
+  });
+  it("Loads correct elements", () => {
+    cy.get('[data-cy="close-modal-button"]');
+    cy.get('[data-cy="instructions-panel-header"]');
+    cy.get('[data-cy="instructions-panel-body"]');
+    cy.get('[data-cy="character-name-paragraph"]');
+    cy.get('[data-cy="character-description-paragraph"]');
+    cy.get('[data-cy="character-dropdown"]');
+    cy.get('[data-cy="submit-button"]');
+  });
+});
+
+describe("Submits the html_static_task", () => {
+  it("Closing starting modal", () => {
+    cy.get('[data-cy="close-modal-button"]').as("modalButton").click();
+    cy.get("@modalButton").should("not.be.visible");
+  });
+  it("Select a character name description", () => {
+    cy.get('[data-cy="character-dropdown"]').select("Good");
+    cy.get('[data-cy="character-dropdown"]').select("Bad");
+  });
+  it("Upload a file", () => {
+    cy.fixture("bliss.png").then((fileContent) => {
+      cy.get('[data-cy="character-file-input"]').attachFile({
+        fileContent: fileContent.toString(),
+        fileName: "bliss.png",
+        mimeType: "image/png",
+      });
+    });
+  });
+  it("Submit the task", () => {
+    cy.on("window:alert", (txt) => {
+      expect(txt).to.contains(
+        'The task has been submitted! Data: {"rating":"bad","file1":{}}'
+      );
+    });
+    cy.intercept({ pathname: "/submit_task" }).as("submitTask");
+    cy.get('[data-cy="submit-button"]').click();
+    cy.wait("@submitTask").then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+    });
+  });
+});
