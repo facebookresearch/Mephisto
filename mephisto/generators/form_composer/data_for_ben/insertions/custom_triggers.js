69161fcee1aa959696c2acbbd0caa4d779c17c8b
Paul Abumov <pavelabumov@meta.com>
2024-08-15 14:45:49 -0400

    Example task config for Ben

---
+++ mephisto/generators/form_composer/data_for_ben/insertions/custom_triggers.js
@@ -0,0 +1,5 @@
+/**
+ * WARNING: Do not remove this file.
+ * It is required for an import that can't be try-catched during the build or run time.
+ * You should place your custom triggers here below this comment
+ */
